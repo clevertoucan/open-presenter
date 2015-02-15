2f2f7df38755b40de3eb317b307fe5506e8d5881
clevertoucan <clevertoucan@gmail.com>
2015-02-15 12:40:29 -0500

    Made random color background switcher

--- src/video/OpenPresenter.java
+++ src/video/OpenPresenter.java
@@ -7,8 +7,7 @@ public class OpenPresenter {
 	public static void main(String[] args) {
 		Frame f = new Frame();
 		f.setVisible(true);
-		while(true){
-			f.setRandBackColor();
-		}
+		f.getContentPane().setBackground(Color.black);
+		
 	}
 }
